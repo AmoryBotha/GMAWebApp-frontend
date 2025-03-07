9d5c699ba3530aadc5b678afd89db38030ca654c
Amory <abotha181@gmail.com>
2025-03-07 11:14:11 +0200

    back to login ds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Replace this:
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// With:
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import AuthPage from './components/AuthPage/AuthPage';
 import UserPortalPage from './components/UserPortal/UserPortalPage';
 import OwnerPortalPage from './components/OwnerPortal/OwnerPortalPage';
@@ -13,21 +17,20 @@ import DownloadStatementPage from './components/DownloadStatementPage/DownloadSt
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<AuthPage />} />
-                <Route path="/user-portal" element={<UserPortalPage />} />
-                <Route path="/owner-portal" element={<OwnerPortalPage />} />
-                <Route path="/reset-password" element={<ResetPasswordPage />} />
-                <Route path="/user-profile" element={<UserProfile />} />
-                <Route path="/trustee-portal" element={<TrusteePortalPage />} />
-                <Route path="/account-detail-view/:accountId" element={<AccountDetailView />} />
-                <Route path="/levy-account-detail/:levyAccountId/:responsiblePersonId" element={<LevyAccountDetailPage />} />
-                <Route path="/fr-change" element={<FRChangePage />} />
-                <Route path="/download-statement" element={<DownloadStatementPage />} />
-
-            </Routes>
-        </Router>
+        <HashRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/user-portal" element={<UserPortalPage />} />
+        <Route path="/owner-portal" element={<OwnerPortalPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/trustee-portal" element={<TrusteePortalPage />} />
+        <Route path="/account-detail-view/:accountId" element={<AccountDetailView />} />
+        <Route path="/levy-account-detail/:levyAccountId/:responsiblePersonId" element={<LevyAccountDetailPage />} />
+        <Route path="/fr-change" element={<FRChangePage />} />
+        <Route path="/download-statement" element={<DownloadStatementPage />} />
+      </Routes>
+    </HashRouter>
     );
 }
 
